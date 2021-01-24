b67b761a4730e87c5e74ab3ce89a5821e06c478a
Mariatta Wijaya <mariatta.wijaya@uplight.com>
2021-01-23 18:50:18 -0800

    Pass the issue comment as body

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -93,7 +93,7 @@ async def repo_installation_added(event, gh, *args, **kwargs):
             await gh.post(
                 f"{issue_url}/comments",
                 accept="application/vnd.github.v3+json",
-                data=f"RTD project created at {resp['urls']['documentation']} on {resp['created']}",
+                data={'body': f"RTD project created at {resp['urls']['documentation']} on {resp['created']}"},
                 oauth_token=installation_access_token["token"],
             )
             await gh.patch(
